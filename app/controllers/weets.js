865ae5d3d269496bdef52fb4380192ef5ab096ce
NICOLAS ALBERTO HENAO <nicolasalbertohenao@MacBook-Pro-de-WOLOX.local>
2021-05-27 11:44:16 -0500

    Refactor add environment variable and error logging

--- app/controllers/weets.js
+++ app/controllers/weets.js
@@ -1,9 +1,11 @@
 const numberFactsService = require('../services/number_facts');
 const errors = require('../errors');
+const logger = require('../logger');
 
 const getWeet = async (req, res, next) => {
   const weetResponse = await numberFactsService.getNumberFact();
   if (weetResponse.status === 200) return res.status(200).send(weetResponse.data);
+  logger.error(`The service responded with the code ${weetResponse.status}`);
   return next(errors.defaultError(`The service responded with the code ${weetResponse.status}`));
 };
 
